e000912bea01a8b180fef7447828b2a8d94a6017
cheesylazy <83595251+cheesylazy@users.noreply.github.com>
2021-05-04 18:03:18 +0900

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://cheesylazy.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://cheesylazy.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
