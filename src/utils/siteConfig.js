a88acd6226e91d1103e20525c0e0cc5d1ac4902e
cheesylazy <83595251+cheesylazy@users.noreply.github.com>
2021-05-20 17:45:19 +0900

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -6,8 +6,8 @@ module.exports = {
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
-    siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
+    shortTitle: `cheesylazy`, // Used for App manifest e.g. Mobile Home Screen
+    siteIcon: `https://s3.us-west-2.amazonaws.com/secure.notion-static.com/70528486-2593-4f90-b0cf-1f50732c4216/square.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210520%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210520T084443Z&X-Amz-Expires=86400&X-Amz-Signature=fa0c5aec40f7996fdbf618e108321076dbb201adcbab8ed86f9f28d983c42673&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22square.png%22`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
 }
