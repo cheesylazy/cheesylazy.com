cd725b3f0d87509659291e198f8db3967d6d5e2f
Github ID <mkkang.jason@gmail.com>
2021-11-10 21:17:58 +0900

    Update post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,27 +5,6 @@ import { Helmet } from 'react-helmet'
 
 import { Layout } from '../components/common'
 import { MetaData } from '../components/common/meta'
-import React, { useEffect } from 'react';
-
-const Comments = () => {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://cdn.commento.io/js/commento.js';
-        script.async = true;
-
-        const comments = document.getElementById('comments-container');
-        if (comments) comments.appendChild(script);
-    }, []);
-
-    return (
-        <>
-            <div id="comments-container"></div>
-            <div id="commento"></div>
-        </>
-    );
-};
-
-export default Comments;
 
 /**
 * Single post view (/:slug)
